b741ce3d075a099844262f6e26db08c8bc89579c
Navinbala5 <bnavin28@gmail.com>
2017-07-02 22:24:10 +0530

    Add files via upload

---
+++ src/main/java/com/example/elam/android10_4/Frag1.java
@@ -0,0 +1,45 @@
+package com.example.elam.android10_4;
+
+/**
+ * Created by elam on 02-07-2017.
+ */
+
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class Frag1 extends Fragment {
+
+    public Frag1() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_frag1, container, false);
+    }
+
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+    }
+}
